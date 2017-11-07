18018aa806a65f993c3bb29cfdbfbb72f8b23bff
a.nikishin <a.nikishin@fun-box.ru>
2017-11-07 21:20:03 +0400

    Refactor test

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -6,84 +6,55 @@ class TestParser < Minitest::Test
     @parser = Parser.new
   end
 
-  def test_simple_calc
-    code = "(+ 1 202 5 7)"
+  def evaluate(code)
+    Interpreter.evaluate(@parser.string_to_ast(code), @global_env)
+  end
 
-    assert_equal 215, Interpreter.evaluate(@parser.string_to_ast(code), @global_env)
+  def test_simple_calc
+    assert_equal 215, evaluate("(+ 1 202 5 7)")
   end
 
   def test_nested_calc
-    code = "(+ 10 3 (/ 6 3) (+ 5 8))"
-
-    assert_equal 28, Interpreter.evaluate(@parser.string_to_ast(code), @global_env)
+    assert_equal 28, evaluate("(+ 10 3 (/ 6 3) (+ 5 8))")
   end
 
   def test_defining
-    code = "((def abc (* 5 5))
-    (+ 2 8 (/ 6 3) (+ 5 abc)))"
-
-    assert_equal 42, Interpreter.evaluate(@parser.string_to_ast(code), @global_env)
+    assert_equal 42, evaluate("((def abc (* 5 5))
+      (+ 2 8 (/ 6 3) (+ 5 abc)))")
   end
 
   def test_bin_pred
-    code = "(>= (/ 8 2) (+ 30 300))"
-    assert_equal false, Interpreter.evaluate(@parser.string_to_ast(code), @global_env)
-
-    code = "(< (* 50 0) 5)"
-    assert_equal true, Interpreter.evaluate(@parser.string_to_ast(code), @global_env)
+    assert_equal false, evaluate("(>= (/ 8 2) (+ 30 300))")
+    assert_equal true, evaluate("(< (* 50 0) 5)")
   end
 
   def test_strings
-    code = "((def abc 'my lisp')(return abc))"
-    assert_equal 'my lisp', Interpreter.evaluate(@parser.string_to_ast(code), @global_env)
-
-    code = '((def abc "my (lisp)")(return abc))'
-    assert_equal "my (lisp)", Interpreter.evaluate(@parser.string_to_ast(code), @global_env)
+    assert_equal 'my (lisp)', evaluate("((def abc 'my (lisp)')(return abc))")
   end
 
   def test_car
-    code = "(car (cons (+ 2 2) (cons 1 (quote ()))))"
-    ast = @parser.string_to_ast(code)
-    assert_equal 4, Interpreter.evaluate(ast, @global_env)
+    assert_equal 4, evaluate("(car (cons (+ 2 2) (cons 1 (quote ()))))")
   end
 
   def test_cdr
-    code = "(car (cdr (cons (+ 2 1) (cons (/ 8 4) (quote ())))))"
-    ast = @parser.string_to_ast(code)
-    assert_equal 2, Interpreter.evaluate(ast, @global_env)
-  end
-
-  def test_quote
-    code = "(+ (cdr (quote (1 2 3 4 5))))"
-    ast = @parser.string_to_ast(code)
-    # Printer.print(Interpreter.evaluate(ast, @global_env))
-    # assert_equal 14, Interpreter.evaluate(ast, @global_env)
+    assert_equal 2, evaluate("(car (cdr (cons (+ 2 1) (cons (/ 8 4) (quote ())))))")
   end
 
   def test_if
-    code = "(if (> 1 2) 1 'False')"
-    ast = @parser.string_to_ast(code)
-    assert_equal "False", Interpreter.evaluate(ast, @global_env)
+    assert_equal "False", evaluate("(if (> 1 2) 1 'False')")
   end
 
   def test_lambda
-    code = "((lambda (x) (+ x 1)) 10)"
-    ast = @parser.string_to_ast(code)
-    assert_equal 11, Interpreter.evaluate(ast, @global_env)
+    assert_equal 11, evaluate("((lambda (x) (+ x 1)) 10)")
   end
 
   def test_fibonachi
-    code = "((def fibo (lambda (n) (if (<= n 1) n (+ (fibo (- n 1)) (fibo (- n 2))))))
-            (fibo 15))"
-    ast = @parser.string_to_ast(code)
-    assert_equal 610, Interpreter.evaluate(ast, @global_env)
+    assert_equal 610, evaluate("((def fibo (lambda (n) (if (<= n 1) n (+ (fibo (- n 1)) (fibo (- n 2))))))
+            (fibo 15))")
   end
 
   def test_validation
-    code = "(+ 1 3 / 6 3) (+ 5 8))"
-    assert_equal false, @parser.string_to_ast(code)
-
-    code = "(+ 1 3)("
-    assert_equal false, @parser.string_to_ast(code)
+    assert_equal false, evaluate("(+ 1 3 / 6 3) (+ 5 8))")
+    assert_equal false, evaluate("(+ 1 3)(")
   end
 end
